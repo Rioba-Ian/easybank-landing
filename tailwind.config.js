8b83864aa14e1c4decbcd7155ac2ba2691565b04
Rioba-Ian <riobafelix@gmail.com>
2024-03-08 16:29:32 +0300

    bring assets set up theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
  theme: {
-  extend: {},
+  extend: {
+   colors: {
+    "dark-blue": "hsl(233, 26%, 24%)",
+    "lime-green": "hsl(136, 65%, 51%)",
+    "bright-cyan": "hsl(192, 70%, 51%)",
+    "greyish-blue": "hsl(233, 8%, 62%)",
+    "light-greyish-blue": "hsl(220, 16%, 96%)",
+    "very-light-gray": "hsl(0, 0%, 98%)",
+   },
+  },
  },
  plugins: [],
 };
